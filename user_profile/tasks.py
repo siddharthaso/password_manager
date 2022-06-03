b1b89323b1d9fd83ee2ddc4b8d8f24750352d747
siddharthaso <siddasodariya10127@gmail.com>
2022-06-03 16:37:49 +0530

    celery

---
+++ user_profile/tasks.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import, unicode_literals
+from celery import shared_task
+from django.core.mail import send_mail
+
+@shared_task(bind = True)
+def sending_mail(self,subject, message, email_from, recipient_list):
+    send_mail(subject, message, email_from, recipient_list)

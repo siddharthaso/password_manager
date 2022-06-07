22c7062b852817b69eeb0e7cba732d92df6e2f5c
siddharthaso <siddasodariya10127@gmail.com>
2022-06-07 15:59:52 +0530

    stop sending email

--- user_profile/tasks.py
+++ user_profile/tasks.py
@@ -4,4 +4,5 @@ from django.core.mail import send_mail
 
 @shared_task(bind = True)
 def sending_mail(self,subject, message, email_from, recipient_list):
-    send_mail(subject, message, email_from, recipient_list)
+    # send_mail(subject, message, email_from, recipient_list)
+    print("sended mail because of password of email i can't send with heroku")
